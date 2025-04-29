d13bd8f0346b36528a3943939e575ba41f0e679b
sinan12344 <150492820+sinan12344@users.noreply.github.com>
2025-04-29 12:56:35 +0300

    Add project files.

---
+++ sistemgiris/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace sistemgiris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "admin";
+            string password = "123456";
+            // hocam izninizle ben while ı ögrendim ve kendimi test etmek için goto ile degil while ile loopa almak istedim burayı. goto yu digerlerinde kullanmıştım.
+            while (true)
+            {
+                Console.WriteLine("Kullanıcı adı giriniz.");
+
+                string ad = Console.ReadLine();
+
+                Console.WriteLine("Şifre giriniz");
+
+                string sifre = Console.ReadLine();
+
+                if (ad == username && sifre == password)
+                {
+                    Console.WriteLine("giriş başarılı");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("lütfen bilgileriniz kontrol ediniz");
+                }
+            }
+        }
+    }
+}
